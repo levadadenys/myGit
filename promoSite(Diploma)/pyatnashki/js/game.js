fc6840e3d504a08f15235072f1eb3875042d66fa
Denys Levada <levada.denys@gmail.com>
2016-12-24 02:11:26 +0200

    diploma/pyatnaski refactored up to ES6

--- promoSite(Diploma)/pyatnashki/js/game.js
+++ promoSite(Diploma)/pyatnashki/js/game.js
@@ -1,102 +1,75 @@
-function Game() {
-    var arr = [1, 2, 3, 4, 5, 6, 7, 8];
-    this.correctArr = [1, 2, 3, 4, 5, 6, 7, 8, 0];
-    this.box = [];
+class Game {
+    constructor() {
+        this.box = [];
 
-    this.arrangeBox = function() {
+        this._correctArr = [1, 2, 3, 4, 5, 6, 7, 8, ' '];
+        this._downExceptions = [0, 1, 2];
+        this._upExceptions = [6, 7, 8];
+        this._rightExceptions = [0, 3, 6];
+        this._leftExceptions = [2, 5, 8];
+    }
 
-        while (this.box.length < this.correctArr.length) {
-            var num = Math.floor(Math.random() * 9);
-            var check = 0;
-            for (var j = 0; j < this.box.length; j++) {
-                if (num != this.box[j]) {
-                    check++;
-                }
-            }
-            if (check === this.box.length) {
-                this.box.push(num);
-            }
+    arrangeBox() {
+        while (this.box.length < this._correctArr.length) {
+            let num = this._correctArr[Math.floor(Math.random() * 9)];
+            let isIncludes = this.box.indexOf(num) + 1;
+            if (!isIncludes) this.box.push(num);
         }
     }
 
-    this.fillBox = function() {
-        for (var i = 0; i < this.box.length; i++) {
-            if (this.box[i] === 0) {
-                document.getElementById(i + 1).innerHTML = ' ';
-            } else {
-                document.getElementById(i + 1).innerHTML = this.box[i];
-            }
+    fillBox() {
+        this.box.map((item, i) => document.getElementById(i).innerHTML = item);
+        this.checkBox();
+    }
+
+    checkBox() {
+        if (this.box.join(' ') === this._correctArr.join(' ')) alert('You win!!!');
+    }
+
+    _isUnmovable(way) {
+        for (let i = 0; i < way.length; i++) {
+            if (this.box[way[i]] === ' ') return true;
         }
     }
 
-    this.checkBox = function() {
-        var checkArr = [];
-        var check = 0;
-        for (var i = 0; i < this.correctArr.length; i++) {
-            if (this.box[i] === ' ') {
-                this.box[i] = 0;
-            }
-            if (this.correctArr[i] === this.box[i]) {
-                check++;
-            }
-        }
-        if (check === this.correctArr.length) {
-            alert('You win!!!');
-        }
-    }
+    controls_down() {
+        let empty = this.box.indexOf(' ');
+        if (!this._isUnmovable(this._downExceptions)) {
+            [this.box[empty], this.box[empty - 3]] = [this.box[empty - 3], this.box[empty]];
 
-    this.controls_down = function() {
-
-        var empty = this.box.indexOf(0);
-        if (empty === 0 || empty === 1 || empty === 2) {
-            return;
-        } else {
-            var change = this.box[empty - 3];
-            this.box[empty] = change;
-            this.box[empty - 3] = ' ';
             this.fillBox();
-            setTimeout(this.checkBox(), 1000);
         }
+    }
+    controls_up() {
+        let empty = this.box.indexOf(' ');
+        if (!this._isUnmovable(this._upExceptions)) {
+            [this.box[empty], this.box[empty + 3]] = [this.box[empty + 3], this.box[empty]];
 
-    }
-    this.controls_up = function() {
-        var empty = this.box.indexOf(0);
-        if (empty === 6 || empty === 7 || empty === 8) {
-            return;
-        } else {
-            var change = this.box[empty + 3];
-            this.box[empty] = change;
-            this.box[empty + 3] = ' ';
             this.fillBox();
-            setTimeout(this.checkBox(), 1000);
         }
     }
+    controls_right() {
+        let empty = this.box.indexOf(' ');
+        if (!this._isUnmovable(this._rightExceptions)) {
+            [this.box[empty], this.box[empty - 1]] = [this.box[empty - 1], this.box[empty]];
 
-    this.controls_right = function() {
-        var empty = this.box.indexOf(0);
-        if (empty === 0 || empty === 3 || empty === 6) {
-            return;
-        } else {
-            var change = this.box[empty - 1];
-            this.box[empty] = change;
-            this.box[empty - 1] = ' ';
             this.fillBox();
-            setTimeout(this.checkBox(), 1000);
         }
     }
-    this.controls_left = function() {
-        var empty = this.box.indexOf(0);
-        if (empty === 2 || empty === 5 || empty === 8) {
-            return;
-        } else {
-            var change = this.box[empty + 1];
-            this.box[empty] = change;
-            this.box[empty + 1] = ' ';
-            this.fillBox();
-            setTimeout(this.checkBox(), 1000);
-        }
-    }
+    controls_left() {
+        let empty = this.box.indexOf(' ');
+        if (!this._isUnmovable(this._leftExceptions)) {
+            [this.box[empty], this.box[empty + 1]] = [this.box[empty + 1], this.box[empty]];
 
+            this.fillBox();
+        }
+    }
 }
 
-var game = new Game();
+let game = new Game();
+
+let start_resetGame = () => {
+    game.box = [];
+    game.arrangeBox();
+    game.fillBox();
+}
